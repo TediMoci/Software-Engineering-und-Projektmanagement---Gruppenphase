be26486de28fce6193414da4b6667703ada030cd
Eva-Maria Resch <e1633055@student.tuwien.ac.at>
2019-05-07 00:54:35 +0200

    bugfix: minor changes in dude mapper class (Iceberg 1.1.1/Issue #36079)

--- sepm-groupphase/backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/mapper/message/actors/DudeMapper.java
+++ sepm-groupphase/backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/mapper/message/actors/DudeMapper.java
@@ -2,29 +2,16 @@ package at.ac.tuwien.sepm.groupphase.backend.entity.mapper.message.actors;
 
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.actors.DudeDto;
 import at.ac.tuwien.sepm.groupphase.backend.entity.Dude;
-import org.mapstruct.Qualifier;
 import org.springframework.stereotype.Component;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
 @Component
 public class DudeMapper {
-
-    @Qualifier
-    @Target(ElementType.METHOD)
-    @Retention(RetentionPolicy.SOURCE)
-    public @interface DudeMapping {
-    }
 
     /**
      *
      * @param dudedto
      * @return
      */
-    @DudeMapping
     public Dude dudeDtoToDude(DudeDto dudedto){
 
         Dude.DudeBuilder builder = new Dude.DudeBuilder();
@@ -55,7 +42,6 @@ public class DudeMapper {
      * @param dude
      * @return
      */
-    @DudeMapping
     public DudeDto dudeToDudeDto(Dude dude){
 
         DudeDto.DudeDtoBuilder builder = new DudeDto.DudeDtoBuilder();
--- sepm-groupphase/backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/mapper/message/actors/IDudeMapper.java
+++ sepm-groupphase/backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/mapper/message/actors/IDudeMapper.java
@@ -3,15 +3,12 @@ package at.ac.tuwien.sepm.groupphase.backend.entity.mapper.message.actors;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.actors.DudeDto;
 import at.ac.tuwien.sepm.groupphase.backend.entity.Dude;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = DudeMapper.class)
 public interface IDudeMapper {
 
-    @Mapping(source = "dudeDto", target = "dude", qualifiedBy = DudeMapper.DudeMapping.class)
     Dude dudeDtoToDude(DudeDto dudedto);
 
-    @Mapping(source = "dude", target = "dudeDto", qualifiedBy = DudeMapper.DudeMapping.class)
     DudeDto dudeToDudeDto(Dude dude);
 
 }
