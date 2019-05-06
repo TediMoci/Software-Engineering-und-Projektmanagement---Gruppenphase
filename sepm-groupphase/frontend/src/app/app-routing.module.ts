7cf826f7bb3cda084e9d80fd3b5b82a38c7da0bd
Amir El Agrod <e1631844@student.tuwien.ac.at>
2019-05-06 12:57:30 +0200

    added register component for frontend (Iceberg 1.1.3/Issue #36148)

--- sepm-groupphase/frontend/src/app/app-routing.module.ts
+++ sepm-groupphase/frontend/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
 import {AuthGuard} from './guards/auth.guard';
 import {MessageComponent} from './components/message/message.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent },
   {path: 'message', canActivate: [AuthGuard], component: MessageComponent}
 ];
 
---
+++ sepm-groupphase/frontend/src/app/components/register/register.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {AuthService} from '../../services/auth.service';
+import {AuthRegister} from '../../dtos/auth-register';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+
+  constructor(  private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+}
