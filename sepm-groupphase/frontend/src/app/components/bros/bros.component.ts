e276530773e236ea659af1c6dff0cec4a7ed43aa
Aaron Beigelbeck <e01529382@student.tuwien.ac.at>
2019-05-10 14:13:17 +0200

    Added components for header-links in frontend (Iceberg 1.7) (Task #36752)

---
+++ sepm-groupphase/frontend/src/app/components/bros/bros.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bros',
+  templateUrl: './bros.component.html',
+  styleUrls: ['./bros.component.scss']
+})
+export class BrosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ sepm-groupphase/frontend/src/app/components/my-content/my-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-content',
+  templateUrl: './my-content.component.html',
+  styleUrls: ['./my-content.component.scss']
+})
+export class MyContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
