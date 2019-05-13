72a77cc1beb2acdd74fe18dee081f5108153c5b9
TediMoci <e01428862@student.tuwien.ac.at>
2019-05-13 08:46:17 +0200

    1.9) DudeIntegrationTest -> TestFindAll

--- sepm-groupphase/backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/integrationtest/DudeIntegrationTest.java
+++ sepm-groupphase/backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/integrationtest/DudeIntegrationTest.java
@@ -3,9 +3,16 @@ package at.ac.tuwien.sepm.groupphase.backend.integrationtest;
 
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.actors.DudeEndpoint;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.actors.DudeDto;
+import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.message.SimpleMessageDTO;
 import at.ac.tuwien.sepm.groupphase.backend.entity.Dude;
 import at.ac.tuwien.sepm.groupphase.backend.exception.ServiceException;
+import at.ac.tuwien.sepm.groupphase.backend.integrationtest.base.BaseIntegrationTest;
+import at.ac.tuwien.sepm.groupphase.backend.repository.actors.IDudeRepository;
 import at.ac.tuwien.sepm.groupphase.backend.service.implementation.actors.DudeService;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,29 +21,37 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles(profiles = "integration-test")
-public class  DudeIntegrationTest {
+public class  DudeIntegrationTest extends BaseIntegrationTest {
+    private static final String DUDE_ENDPOINT = "/dudes";
     private static DudeDto testDude = new DudeDto();
 
     @MockBean
-    DudeService dudeService;
+    IDudeRepository dudeRepository;
 
     @Autowired
     DudeEndpoint dudeEndpoint;
 
     @Before
     public void setUp() throws Exception{
-        MockitoAnnotations.initMocks(this);
+        testDude.setId(1L);
         testDude.setName("John");
         testDude.setPassword("123456789");
         testDude.setEmail("john1@dude.com");
@@ -49,6 +64,45 @@ public class  DudeIntegrationTest {
         testDude.setWeight(85.0);
     }
 
+    @Test
+    public void TestFindAll() throws ServiceException{
+        Dude Dude1 = new Dude();
+        Dude1.setId(1L);
+        Dude1.setName("John");
+        Dude1.setPassword("123456789");
+        Dude1.setEmail("john1@dude.com");
+        Dude1.setDescription("Description 1");
+        Dude1.setBirthday(LocalDate.of(1982,1,1));
+        Dude1.setSex('M');
+        Dude1.setStatus(1);
+        Dude1.setSelfAssessment(1);
+        Dude1.setHeight(185.0);
+        Dude1.setWeight(85.0);
+
+        List<Dude> dudes = new ArrayList<>();
+        dudes.add(Dude1);
+        dudes.add(Dude1);
+
+        List<DudeDto> dudesDto = new ArrayList<>();
+        dudesDto.add(testDude);
+        dudesDto.add(testDude);
+
+        Mockito.when(dudeRepository.findAll()).thenReturn(dudes);
+
+        Response response = RestAssured
+            .given()
+            .contentType(ContentType.JSON)
+            .header(HttpHeaders.AUTHORIZATION, validUserTokenWithPrefix)
+            .when().get(DUDE_ENDPOINT+"/all")
+            .then().extract().response();
+
+        Assert.assertThat(response.getStatusCode(), is(HttpStatus.OK.value()));
+
+        List<DudeDto> dudesDto2 = Arrays.asList(response.as(DudeDto[].class));
+        Assert.assertThat(dudesDto2, is(dudesDto));
+    }
+
+/**
     @Test
     public void TestFindByNameAndPassword() throws ServiceException {
         Dude Dude1 = new Dude();
@@ -66,5 +120,5 @@ public class  DudeIntegrationTest {
         Mockito.when(dudeService.findByNameAndPassword(anyString(),anyString())).thenReturn(Dude1);
         assertEquals(dudeEndpoint.findByNameAndPassword("John","John's Password"), testDude);
 
-    }
+    } **/
 }
