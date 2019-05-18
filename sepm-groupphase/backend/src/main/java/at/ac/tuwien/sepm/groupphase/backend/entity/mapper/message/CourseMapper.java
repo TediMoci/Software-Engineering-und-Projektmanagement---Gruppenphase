84036606d4e49b6dac5fdb6d6dd97731bdcf3146
Aaron Beigelbeck <e01529382@student.tuwien.ac.at>
2019-05-18 12:20:03 +0200

    Implemented save and find functions for Course (Iceberg 3.1; 3.2/ Issue #37784; #37820)

--- sepm-groupphase/backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/mapper/message/CourseMapper.java
+++ sepm-groupphase/backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/mapper/message/CourseMapper.java
@@ -31,12 +31,17 @@ public class CourseMapper implements ICourseMapper {
         builder.id(courseDto.getId());
         builder.name(courseDto.getName());
         builder.description(courseDto.getDescription());
+        // TODO: fix mapping
+        builder.fitnessProvider(null);
+        builder.dudes(null);
+        /*
         builder.fitnessProvider(fitnessProviderMapper.fitnessProviderDtoToFitnessProvider(courseDto.getFitnessProviderDto()));
         Set<Dude> dudes = new HashSet<>();
         for (DudeDto dudeDto : courseDto.getDudeDtos()) {
             dudes.add(dudeMapper.dudeDtoToDude(dudeDto));
         }
         builder.dudes(dudes);
+         */
 
         return builder.build();
     }
@@ -48,12 +53,17 @@ public class CourseMapper implements ICourseMapper {
         builder.id(course.getId());
         builder.name(course.getName());
         builder.description(course.getDescription());
+        // TODO: fix mapping
+        builder.fitnessProviderDto(null);
+        builder.dudeDtos(null);
+        /*
         builder.fitnessProviderDto(fitnessProviderMapper.fitnessProviderToFitnessProviderDto(course.getFitnessProvider()));
         Set<DudeDto> dudeDtos = new HashSet<>();
         for (Dude dude : course.getDudes()) {
             dudeDtos.add(dudeMapper.dudeToDudeDto(dude));
         }
         builder.dudeDtos(dudeDtos);
+         */
 
         return builder.build();
     }
--- sepm-groupphase/backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/ICourseRepository.java
+++ sepm-groupphase/backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/ICourseRepository.java
@@ -1,9 +1,35 @@
 package at.ac.tuwien.sepm.groupphase.backend.repository;
 
 import at.ac.tuwien.sepm.groupphase.backend.entity.Course;
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ICourseRepository extends JpaRepository<Course, Long> {
+
+    /**
+     * @param course
+     * @return
+     * @throws DataAccessException
+     */
+    Course save(Course course) throws DataAccessException;
+
+    /**
+     * @param name
+     * @return
+     * @throws DataAccessException
+     */
+    @Query("SELECT c FROM Course c WHERE c.name LIKE ?1%")
+    List<Course> findByName(String name) throws DataAccessException;
+
+    /**
+     * @return
+     * @throws DataAccessException
+     */
+    List<Course> findAll() throws DataAccessException;
+
 }
