f1603f907e4d0515950caa02a9ed2e6c9fe0472f
Oskar <oskar.perl@gmx.at>
2019-05-23 22:50:36 +0200

    Searchbar now takes input correctly, Buttons now switch category correctly, category is now displayed in Searchbar preview(4.2 / #37792)

--- sepm-groupphase/frontend/src/app/components/find/find.component.ts
+++ sepm-groupphase/frontend/src/app/components/find/find.component.ts
@@ -8,7 +8,8 @@ import {Dude} from '../../dtos/dude';
   styleUrls: ['./find.component.scss']
 })
 export class FindComponent implements OnInit {
-
+  category: string = "Exercise";
+  public inputText: any;
   imagePath: string = '/assets/img/kugelfisch.jpg';
   userName: string;
   dude: Dude;
@@ -19,6 +20,8 @@ export class FindComponent implements OnInit {
   ngOnInit() {
     this.dude = JSON.parse(localStorage.getItem('loggedInDude'));
     this.userName = this.dude.name;
+    console.log("searchvalue: " + this.inputText)
+
   }
 
 }
